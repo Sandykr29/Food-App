c855228f31797f7a976928c098e8457d63a9463b
sandykr29 <sandeepkumar29774@yahoo.com>
2024-03-16 22:21:58 +0530

    a

--- foodapp/src/App.js
+++ foodapp/src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+import { Route,Routes } from 'react-router-dom';
 import './App.css';
 import { Navbar } from './components/Navbar/Navbar';
+import { Home } from './components/pages/Home/Home';
 
 function App() {
   return (
     <div className="app">
       <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ foodapp/src/components/pages/PlaceOrder/PlaceOrder.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const PlaceOrder = () => {
+  return (
+    <div>PlaceOrder</div>
+  )
+}
