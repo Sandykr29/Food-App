e499d73293228914b4ae7fbecfb8b51e4b529a43
sandykr29 <sandeepkumar29774@yahoo.com>
2024-03-17 23:51:10 +0530

    a

--- foodapp/src/App.js
+++ foodapp/src/App.js
@@ -4,6 +4,7 @@ import { Navbar } from './components/Navbar/Navbar';
 import { Home } from './components/pages/Home/Home';
 
 function App() {
+
   return (
     <div className="app">
       <Navbar/>
