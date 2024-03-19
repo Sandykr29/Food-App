842a318e5333736d33a9fd552db13af01114b0bd
sandykr29 <sandeepkumar29774@yahoo.com>
2024-03-19 23:54:00 +0530

    context

--- foodapp/src/context/StoreContext.jsx
+++ foodapp/src/context/StoreContext.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { createContext } from 'react';
+import { food_list } from '../assets/assets';
 
 export const StoreContext=createContext(null)
 
 export const StoreContextProvider = (props) => {
 
 const contextValue={
-    
+    food_list
 }
 
   return (
