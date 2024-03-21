de064d39bde90a6e1bf4758fe5cf4228c8bfb98a
sandykr29 <sandeepkumar29774@yahoo.com>
2024-03-22 02:08:19 +0530

    cart-working

--- foodapp/src/components/Navbar/Navbar.jsx
+++ foodapp/src/components/Navbar/Navbar.jsx
@@ -2,27 +2,28 @@ import React from 'react'
 import "./Navbar.css"
 import { useState } from 'react';
 import { assets } from '../../assets/assets';
+import { Link } from 'react-router-dom';
 
-export const Navbar = () => {
+export const Navbar = ({setShowLogin}) => {
 
     const [menu,setMenu]=useState("home")
 
   return (
     <div className='navbar'>
-        <img src={assets.logo} className='logo' />
+        <Link to="/"><img src={assets.logo} className='logo' /></Link>
         <ul className='navbar-menu'>
-            <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</li>
-            <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</li>
-            <li onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</li>
-            <li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</li>
+           <Link to='/' onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</Link>
+            <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</a>
+            <a href='#app-download' onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</a>
+            <a href='#footer' onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</a>
         </ul>
         <div className="navbar-right">
             <img src={assets.search_icon} alt="" />
             <div className="navbar-search-icon">
-                <img src={assets.basket_icon} alt="" />
+                <Link to={"/cart"}><img src={assets.basket_icon} alt="" /></Link>
                 <div className="dot"></div>
             </div>
-            <button>sign in</button>
+            <button onClick={()=>setShowLogin(true)}>sign in</button>
         </div>
     </div>
   )
--- foodapp/src/pages/Cart/Cart.jsx
+++ foodapp/src/pages/Cart/Cart.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import "./Cart.css"
+import { StoreContext } from '../../context/StoreContext'
+import { food_list } from '../../assets/assets'
 
 export const Cart = () => {
+  const {cartItems,foot_list,removeFromCart}=useContext(StoreContext)
   return (
-    <div>Cart</div>
+    <div className='cart'>
+      <div className="cart-items">
+      <div className="cart-items-title">
+      <p>Items</p>
+      <p>Title</p>
+      <p>Price</p>
+      <p>Quantity</p>
+      <p>Total</p>
+      <p>Remove</p>
+      </div>
+      </div>
+      <br />
+      <hr />
+{food_list.map((item,index)=>{
+  if(cartItems[item._id]>0){
+    return (
+
+      <div>
+           <div className='cart-items-title cart-items-item'>
+            <img src={item.image} alt="" />
+            <p>{item.name}</p>
+            <p>{item.price}</p>
+            <p>{cartItems[item._id]}</p>
+            <p>{item.price*cartItems[item._id]}</p>
+            <p>x</p>
+           </div>
+           <hr />
+      </div>
+      
+    )
+  }
+})}
+    </div>
   )
 }
