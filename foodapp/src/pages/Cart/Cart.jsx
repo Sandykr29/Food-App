53c23dd14867d63469b74f3ec5de43f0dd34142f
sandykr29 <sandeepkumar29774@yahoo.com>
2024-03-23 00:55:59 +0530

    cart

--- foodapp/src/pages/Cart/Cart.jsx
+++ foodapp/src/pages/Cart/Cart.jsx
@@ -8,17 +8,16 @@ export const Cart = () => {
   return (
     <div className='cart'>
       <div className="cart-items">
-      <div className="cart-items-title">
-      <p>Items</p>
-      <p>Title</p>
-      <p>Price</p>
-      <p>Quantity</p>
-      <p>Total</p>
-      <p>Remove</p>
-      </div>
-      </div>
-      <br />
-      <hr />
+          <div className="cart-items-title">
+              <p>Items</p>
+              <p>Title</p>
+              <p>Price</p>
+              <p>Quantity</p>
+              <p>Total</p>
+              <p>Remove</p>
+          </div>
+     <br />
+     <hr />
 {food_list.map((item,index)=>{
   if(cartItems[item._id]>0){
     return (
@@ -33,11 +32,42 @@ export const Cart = () => {
             <p onClick={()=>removeFromCart(item._id)} className='cross'>x</p>
            </div>
            <hr />
-      </div>
-      
+      </div>  
     )
   }
 })}
+  </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>{0}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>{2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>{0}</b>
+            </div>
+          </div>
+          <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cart-promocode">
+          <div>
+            <p>If you have a promo code, Enter it here</p>
+            <div className="cart-promocode-input">
+              <input type="text" placeholder='promo code' />
+              <button>Submit</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
